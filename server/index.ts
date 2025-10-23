6a10511d75e813f0cbb15177d3d706c366357353
jonayeds <jonayeeds@gmail.com>
2025-10-23 17:57:14 +0600

    swiched to synchronus route from asyncronus

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,9 @@ import { QdrantVectorStore } from "@langchain/qdrant";
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
 import { ChatGroq } from "@langchain/groq";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Document } from "@langchain/core/documents";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
+import { PDFParse } from "pdf-parse";
 
 const llm = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
@@ -35,14 +38,30 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
-  await queue.add(
-    "file-ready",
-    JSON.stringify({
-      filename: req.file?.filename,
-      source: req.file?.destination,
-      path: req.file?.path,
-    })
+  const { filename, path, destination: source } = req?.file!;
+  console.log(`Processing file ${filename} from ${source} at ${path}`);
+
+  const parser = new PDFParse({ url: path });
+  const doc = (await parser.getText()).text;
+  const textSplitter = new CharacterTextSplitter({
+    chunkSize: 300,
+    chunkOverlap: 0,
+  });
+  const texts = await textSplitter.splitText(doc);
+  const embeddings = new HuggingFaceInferenceEmbeddings({
+    apiKey: process.env.HUGGINGFACE_API_KEY,
+    model: "BAAI/bge-base-en-v1.5",
+  });
+  const docs = texts.map((t) => new Document({ pageContent: t }));
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: "http://localhost:6333",
+      collectionName: "pdf-collection",
+    }
   );
+  await vectorStore.addDocuments(docs);
+  console.log("all docs are added to vector database");
   res.json({ message: "File uploaded successfully" });
 });
 
@@ -66,6 +85,7 @@ app.get("/chat", async (req, res) => {
   const SYSTEM_PROMPT = `
     You are a helpful assistant that canswers questions about the user query based on the available context from pdf files
     Context\n${JSON.stringify(result).replace(/{/g, "[").replace(/}/g, "]")}
+    Give the resesponse in string format only. keep the heading inside ** **  and subheadings inside * *   
     `;
   const prompt = ChatPromptTemplate.fromMessages([
     {
